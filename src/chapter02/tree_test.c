e53f54d19ea13559c9300b9f5cb2ae00169358c2
Patrick Bucher <patrick.bucher@mailbox.org>
2020-10-31 09:08:02 +0100

    Exercise 2-13: added assertions and more test cases

--- src/chapter02/tree_test.c
+++ src/chapter02/tree_test.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "tree.h"
@@ -7,10 +8,16 @@ void printnv(Nameval *node, char *fmt)
 	printf(fmt, node->name, node->value);
 }
 
+void add_points(Nameval *node, int *sum)
+{
+    *sum += node->value;
+}
+
 int main()
 {
 	Nameval *tree, *node;
 	Nameval *ham, *bot, *ver, *ric, *per, *nor, *alb;
+    int points;
 
 	ham = create("Hamilton", 230);
 	bot = create("Bottas", 161);
@@ -29,6 +36,22 @@ int main()
 	tree = insert(tree, nor);
 	tree = insert(tree, alb);
 
+    assert(lookup(tree, "Hamilton")->value == 230);
+    assert(lookup(tree, "Bottas")->value == 161);
+    assert(lookup(tree, "Verstappen")->value == 147);
+    assert(lookup(tree, "Ricciardo")->value == 78);
+    assert(lookup(tree, "Perez")->value == 68);
+    assert(lookup(tree, "Norris")->value == 65);
+    assert(lookup(tree, "Albon")->value == 64);
+
+    assert(nrlookup(tree, "Hamilton")->value == 230);
+    assert(nrlookup(tree, "Bottas")->value == 161);
+    assert(nrlookup(tree, "Verstappen")->value == 147);
+    assert(nrlookup(tree, "Ricciardo")->value == 78);
+    assert(nrlookup(tree, "Perez")->value == 68);
+    assert(nrlookup(tree, "Norris")->value == 65);
+    assert(nrlookup(tree, "Albon")->value == 64);
+
 	printf("%s: %d\n", tree->name, tree->value);
 
 	node = NULL;
@@ -43,6 +66,14 @@ int main()
 
 	puts("Apply post-order");
 	applypostorder(tree, printnv, "%s: %d\n");
+
+    points = 0;
+    applyinorder(tree, add_points, &points);
+    assert(points == 230 + 161 + 147 + 78 + 68 + 65 + 64);
+
+    points = 0;
+    applypostorder(tree, add_points, &points);
+    assert(points == 230 + 161 + 147 + 78 + 68 + 65 + 64);
 
 	return 0;
 }
